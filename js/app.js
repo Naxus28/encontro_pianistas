908ff9ea80cccbfe822fd0ea0d5cb5045553f705
Gabriel Ferraz <gabrielferraz27@gmail.com>
2015-06-06 19:14:08 -0400

    finished professors page and videos

--- js/app.js
+++ js/app.js
@@ -5,4 +5,26 @@ $(function(){
 	  horizontalOffset: 0,
 	  verticalOffset: 0
 	});
+
+	var app = {
+		openCloseMenu : function(translate) {
+			translate = typeof(translate) != 'undefined' ? $('aside').css({'transform':'translate(0, 0)'}) : $('aside').css({'transform':'translate(100%, 0)'});
+		}
+	}
+
+	/*=======CLICK EVENTS AND EVENT LISTENERS=======*/
+	$('.open_menu').on('click',(function() {
+		app.openCloseMenu(true);
+	}));
+	$('.close_menu').on('click',(function() {
+		app.openCloseMenu();
+	}));
+
+	$('.open_videos').each(function(){
+		$(this).on('click', function(){
+			// alert('works');
+			$(this).closest('.row').siblings('.show_videos').slideToggle('slow');
+		});
+		;
+	});
 });
