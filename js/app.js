9f65e62c95d04b7524cfa71852f576318911e2b6
Gabriel Ferraz <gabrielferraz27@gmail.com>
2015-06-11 19:53:46 -0400

    added contact form

--- js/app.js
+++ js/app.js
@@ -1,6 +1,81 @@
 $(document).foundation();
 
 $(function(){
+	/******************AJAX CALL TO EMAIL.PHP********************/
+	$('form.ajax').on('submit', function(){
+	    var that = $(this),
+	        url = that.attr('action'), //get url for ajax call
+	        type = that.attr('method'), //get method for ajax call
+	        data = {} //initialize data object to store ajax response
+
+
+	    //find all input fields that have the name attribute
+	    that.find('[name]').each(function(){ 
+	        var that = $(this),
+	            name = that.attr('name'), //get the name attribute value
+	            value = that.val(); //get the input value
+	            data[name] = value; //build the data obj i.e. {'name' : 'Gabriel', 'email' : 'gabriel@gabriel.com', 'subject' : 'hello'};
+	    });
+
+	    //make the ajax call
+	    $.ajax({
+	        url: url, //call url
+	        type: type, //send form method
+	        data: data, //send data object
+	        success: function(response){
+	            var obj = $.parseJSON(response); //parse JSON sent by PHP script -- using json_encode on the PHP script
+	            console.log(obj);
+
+	            //check if e-mail was sent and show success alert
+	            if(obj['success'] == true){
+
+	                //animate alert div to display on HTML
+	                $('div.success').css('display', 'block').animate({opacity: 1}, 500);
+	                
+	                //append visitor name to the alert
+	                $('<span>'+obj['name']+'</span>').insertAfter('span.add_name_after');
+
+	                //hide the fail alert--in case user missed a field in a previous form submission
+	                $('div.fail').css('display', 'none');
+
+	                //empty input fields, except the send button
+	                $('form.ajax').find('[name]').each(function(){
+	                    if($(this).val() != 'Send'){
+	                        $(this).val('');
+	                    }
+	                });
+
+	            }
+
+	            //if form validation fails on email.php
+	            else if(obj['success'] == false){
+
+	                //animate alert div to display on HTML
+	                $('div.fail').css('display', 'block').animate({opacity: 1}, 500);
+
+	                //append visitor name to the alert
+	                $('<span>'+obj['name']+'</span>').insertAfter('span.fill_out_form');
+	            }
+
+	            //if e-mail is not sent for any other reason and obj['success'] is not set
+	            else {
+	                $('<div data-alert class="failed_email alert-box alert radius">I am sorry. There was an error and this e-mail could not be sent. Please contact me at <a class="contact_me" href="mailto:gabrielferraz27@gmail.com?Subject=Hello Gabriel!" target="_top">gabrielferraz27@gmail.com</a><a href="#" class="close">&times;</a></div>').insertBefore('form.ajax');
+	                $('div.success').css('display', 'none');
+	                $('div.fail').css('display', 'none');
+	            }
+	        }
+	    })
+	    return false;
+	});
+
+	//Alert closes on "X" click
+
+	$('.close').on('click', function(){
+	    $(this).parent('.alert').animate({opacity: 0}, 500);
+	});
+
+
+	/******************STELLAR PLUGIN********************/
 	$.stellar({
 	  horizontalOffset: 0,
 	  verticalOffset: 0
---
+++ php_scripts/email.php
@@ -0,0 +1,129 @@
+<?php 
+	// session_start();
+	if(isset($_POST['submit'])){
+		//create safe variables to use in the email
+		$name = htmlspecialchars($_POST['name']);
+		$email = htmlspecialchars($_POST['email']);
+		$subject = htmlspecialchars($_POST['subject']);
+		$phone_number = htmlspecialchars($_POST['phone']);
+		$message = htmlspecialchars($_POST['message']);
+
+		//check if all required fields were submitted
+		//if they were not, send user back to contact form and re-populate the fields
+		if(!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['subject']) && !empty($_POST['message']))
+		{
+			$_SESSION['submitted'] = true;
+			$data['submitted'] = $_SESSION['submitted']; 
+		}
+		else
+		{
+			$_SESSION['submitted'] = false;
+			//create session variables to re-populate the form if using php without ajax
+			$_SESSION['name'] = $name;
+			$_SESSION['email'] = $email;
+			$_SESSION['subject'] = $subject;
+			$_SESSION['phone'] = $phone_number;
+			$_SESSION['message'] = $message;
+			
+			//variables to be passed back to ajax
+			$data['success'] = $_SESSION['submitted']; 
+			$data['name'] = $name;
+			echo json_encode($data);
+			exit;
+		}
+	}
+	
+
+	//function that checks script injections
+	function IsInjected($str)
+	{
+	    $injections = array('(\n+)',
+	           '(\r+)',
+	           '(\t+)',
+	           '(%0A+)',
+	           '(%0D+)',
+	           '(%08+)',
+	           '(%09+)'
+	           );
+	                
+	    $inject = join('|', $injections);
+	    $inject = "/$inject/i";
+	     
+	    //perform regular expression match
+	    if(preg_match($inject,$str))
+	    {
+	      return true;
+	    }
+	    else
+	    {
+	      return false;
+	    }
+	}
+
+	//call function
+	if(IsInjected($email))
+	{
+	    echo '<!DOCTYPE html>
+		<html class="no-js" lang="en">
+		  <head>
+		    <meta charset="utf-8" />
+		    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		    <title>E-mail Success</title>
+		    <link rel="stylesheet" href="css/app.css" />
+		    <script src="bower_components/modernizr/modernizr.js"></script>
+		    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+		  </head>
+			<body>
+				<div class="row success">
+					<h1 style="text-align: center;" class="success_noAccess">This is not cool. Bad cookie!</h1>
+				</div>
+
+			<!-- SCRIPT TAGS
+			==================================== -->
+			<script src="bower_components/jquery/dist/jquery.min.js"></script>
+			<script src="bower_components/foundation/js/foundation.min.js"></script>
+			</body>
+		</html>';
+	    exit;
+	}
+
+	//customize message
+	$email_body = "
+
+	Nova mensagem enviada no site eipiano.com.br. 
+
+	REMETENTE: $name
+
+	E-MAIL: $email 
+
+	TELEFONE: $phone_number 
+	                        
+	MENSAGEM: $message.
+
+	";
+
+	//prepare message
+	$to = 'gabrielferraz27@gmail.com';
+	$headers = 'From:  eipiano.com.br' . "\r\n" .
+	'Reply-To:  gabrielferraz27@gmail.com' . "\r\n" .
+	'X-Mailer: PHP/' . phpversion();
+
+	//send email
+	mail($to, $subject, $email_body, $headers);
+
+	//Test if email was sent
+	if( @mail($to, $subject, $email_body, $headers) )
+	{
+		//variables to be passed back to ajax
+		$data['name'] = $name;
+		$data['success'] = true;
+	 	echo json_encode($data);
+		exit;
+	}
+	else
+	{
+		$data['name'] = $name;
+	 	echo json_encode($data);
+		exit;
+	}
+ ?>
